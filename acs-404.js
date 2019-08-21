4c79b1af1a6972abc3866619cd36d05a0f736b11
Kevin Huynh <k.huynh1@yahoo.com>
2019-08-20 23:30:56 -0400

    update

--- acs-404.js
+++ acs-404.js
@@ -2,6 +2,14 @@ var targetOffset = $(".info").offset().top;
 
 var $w = $(window).scroll(function(){
     if ($w.scrollTop() > targetOffset) {
+        $("#logo").css({"fill":"#222"});
+    } else{
+        $("#logo").css({"fill":"#fff"});
+    }
+});
+
+var $e = $(window).scroll(function(){
+    if ($e.scrollTop() > targetOffset) {
         $("#lines path").css({"fill":"#222"});
     } else{
         $("#lines path").css({"fill":"#fff"});
